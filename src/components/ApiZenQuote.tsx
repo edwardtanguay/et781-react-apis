36f8274a52b406da92ed76b50b2740ca7e06efbe
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-21 22:55:16 +0100

    button clicks

--- src/components/ApiZenQuote.tsx
+++ src/components/ApiZenQuote.tsx
@@ -5,9 +5,13 @@ export const ApiZenQuote = () => {
 	const [quote, setQuote] = useState('Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi rerum illum voluptatem numquam, exercitationem mollitia harum quaerat maiores inventore in. Quos nesciunt nihil fugiat repellendus aliquam harum ullam. Vero, aspernatur.');
 	const [author, setAuthor] = useState('Zen');
 
+	const handleFetchQuote = () => {
+		alert('fetching')
+	}
+
 	return (
 		<div className="bg-gray-300 p-6 rounded-lg w-[20rem] flex flex-col gap-4 shadow-md shadow-slate-500">
-			<button>Display a random quote</button>
+			<button onClick={handleFetchQuote}>Display a random quote</button>
 			<p><span className="italic">"{quote}"</span> - {author}</p>
 		</div>
 	)
