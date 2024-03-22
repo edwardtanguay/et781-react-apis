7e0ebbdf789d0fc20d821717ee6981685c204b13
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-22 08:01:53 +0100

    state variable bloat refactored

--- src/components/ApiNasa.tsx
+++ src/components/ApiNasa.tsx
@@ -1,31 +1,37 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useState } from "react"
+import React, { useState } from "react"
 import { ApiCard } from "./ApiCard";
+import { initialNasaData, NasaData } from "../types";
 
 export const ApiNasa = () => {
-	const [date, setDate] = useState('2024-01-01');
-	const [explanation, setExplanation] = useState('');
-	const [imageUrl, setImageUrl] = useState('');
+	const [nasaData, setNasaData] = useState(initialNasaData);
 
 	const key = import.meta.env.VITE_NASA_KEY;
 
 	const handleFetchApodData = () => {
 		(async () => {
-			const url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${date}`;
+			const url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${nasaData.date}`;
 			const response = await fetch(url);
 			const data = await response.json();
-			const _explanation = data.explanation;
-			const _imageUrl = data.url;
-			setExplanation(_explanation);
-			setImageUrl(_imageUrl);
+			const _nasaData = structuredClone(nasaData) as NasaData;
+			_nasaData.explanation = data.explanation;
+			_nasaData.imageUrl = data.url;
+			setNasaData(_nasaData);
 		})();
+	}
+
+	const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value;
+		const _nasaData = structuredClone(nasaData) as NasaData;
+		_nasaData.date = value;
+		setNasaData(_nasaData);
 	}
 
 	return (
 		<ApiCard cardWidth="30rem">
-			<input value={date} onChange={(e) => setDate(e.target.value)} /> <button onClick={handleFetchApodData} >Get astronomy picture</button>
-			<p><img src={imageUrl}/></p>
-			<p>{explanation}</p>
+			<input value={nasaData.date} onChange={(e) => handleDateChange(e)} /> <button onClick={handleFetchApodData} >Get astronomy picture</button>
+			<p><img src={nasaData.imageUrl} /></p>
+			<p>{nasaData.explanation}</p>
 		</ApiCard>
 	)
 }
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export type NasaData = {
+	date: string;
+	explanation: string;
+	imageUrl: string;
+}
+
+export const initialNasaData: NasaData = {
+	date: '2024-02-01',
+	explanation: '',
+	imageUrl: ''
+}
