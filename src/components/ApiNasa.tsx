2c0792b21311692e3e7c366c0f27e2eb0e3a4d0d
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-22 01:10:02 +0100

    key in

--- src/components/ApiNasa.tsx
+++ src/components/ApiNasa.tsx
@@ -6,15 +6,18 @@ export const ApiNasa = () => {
 	const [explanation, setExplanation] = useState('');
 	const [imageUrl, setImageUrl] = useState('');
 
+	const key = import.meta.env.VITE_NASA_KEY;
+
 	const handleFetchApodData = () => {
 		(async () => {
-			const url = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`;
+			const url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${date}`;
 			const response = await fetch(url);
 			const data = await response.json();
 			const _explanation = data.explanation;
 			const _imageUrl = data.url;
 			setExplanation(_explanation);
 			setImageUrl(_imageUrl);
+			console.log(data);
 		})();
 	}
 
